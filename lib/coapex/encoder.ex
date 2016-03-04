07e771b5aed2250d14c132754be51f0cb21f57eb
Geovane Fedrecheski <geonnave@gmail.com>
2016-03-04 16:22:59 -0300

    Refactor code and tests

--- lib/coapex/encoder.ex
+++ lib/coapex/encoder.ex
@@ -1,21 +1,7 @@
 defmodule Coapex.Message do
-  defstruct [
-    version: nil ,# TODO: why this doesnt work: <<1 :: size(2)>>,
-    type: nil,
-    tk_len: nil,
-    code: nil,
-    msg_id: nil,
-    token: nil,
-    options: nil,
-    payload: nil
-  ]
-end
+  defstruct [:type, :token, :code, :msg_id, :options, :payload]
 
-defmodule Coapex.Encoder do
-  alias Coapex.Message
-
-  use Bitwise
-
+  @version <<1::size(2)>>
   @types [con: 0, non: 1, ack: 2, rst: 3]
   @options [
     "If-Match":        1,
@@ -37,23 +23,18 @@ defmodule Coapex.Encoder do
 
   def options, do: @options
   def types, do: @types
+  def version, do: @version
+end
 
-  def build_msg({type, token, code, msg_id, options, payload}) do
-    %Message{version: <<1 :: size(2)>>}
-    |> set_type(type)
-    |> set_token(token)
-    |> set_code(code)
-    |> set_msg_id(msg_id)
-    |> set_options(options)
-    |> set_payload(payload)
-  end
+defmodule Coapex.Encoder do
+  alias Coapex.Message
 
-  def encode(msg = {_type, _token, _code, _msg_id, _options, _payload}) do
-    msg |> build_msg |> encode
-  end
-  def encode(msg = %Message{}) do
-    <<(msg.version)::bitstring, (msg.type)::bitstring, (msg.tk_len)::bitstring, msg.code::bitstring,
-      (msg.msg_id)::bitstring, (msg.options)::bitstring, 0xFF, (msg.payload)::bitstring>>
+  use Bitwise
+
+  def encode(%Message{type: t, code: c, token: tk, msg_id: id, options: opts, payload: p}) do
+    {token, tk_len} = encode_token(tk)
+    <<Message.version()::bitstring, encode_type(t)::bitstring, tk_len::bitstring, encode_code(c)::bitstring,
+      encode_msg_id(id)::bitstring, token::bitstring, encode_options(opts)::bitstring, 0xFF, encode_payload(p)::bitstring>>
   end
 
   @doc """
@@ -61,20 +42,17 @@ defmodule Coapex.Encoder do
     type Confirmable (0), Non-confirmable (1), Acknowledgement (2), or
     Reset (3).
   """
-  def set_type(msg, type) do
-    %Message{msg | type: <<@types[type] :: size(2)>>}
-  end
+  def encode_type(type), do: <<Message.types[type] :: size(2)>>
 
   @doc """
   The Token is used to match a response with a request.  The token
     value is a sequence of 0 to 8 bytes.
   """
-  def set_token(msg, <<>>), do: %Message{msg | tk_len: <<0::unsigned-integer-size(4)>>, token: <<>> }
-  def set_token(msg, token) when is_binary(token) do
-    len = String.length(token)
-    %Message{msg | tk_len: <<len::unsigned-integer-size(4)>>, token: token }
+  def encode_token(<<>>), do: {<<>>, <<0::unsigned-integer-size(4)>>}
+  def encode_token(token) when is_binary(token) do
+    {token, <<String.length(token)::unsigned-integer-size(4)>>}
   end
-  def set_token(msg, _token), do: msg
+  def encode_token(_token), do: raise "Invalid token!"
 
   @doc """
   Code: 8-bit unsigned integer, split into a 3-bit class (most
@@ -82,48 +60,45 @@ defmodule Coapex.Encoder do
     In case of a request, the Code field indicates the Request Method;
     in case of a response, a Response Code.
   """
-  def set_code(msg, code) when is_integer(code) do
-    set_code(msg, {div(code, 100), rem(code, 100)})
+  def encode_code(code) when is_integer(code) do
+    encode_code({div(code, 100), rem(code, 100)})
   end
-  def set_code(msg, << class, ".", detail :: binary>>) do
-    set_code(msg, {class-48, String.to_integer(detail)})
+  def encode_code(<< class, ".", detail :: binary>>) do
+    encode_code({class-48, String.to_integer(detail)})
   end
-  def set_code(msg, {class, detail}) when class in [0, 2, 4, 5] do
-    %Message{msg | code: <<class :: size(3), detail :: size(5)>>}
+  def encode_code({class, detail}) when class in [0, 2, 4, 5] do
+    <<class :: size(3), detail :: size(5)>>
   end
-  def set_code(_msg, _code), do: raise "Invalid code"
+  def encode_code(_code), do: raise "Invalid code"
 
   @doc """
-  Message ID: 16-bit unsigned integer in network byte order.
+  BinaryMessage ID: 16-bit unsigned integer in network byte order.
   """
-  def set_msg_id(msg, id) when id > 0 do
-    %Message{msg | msg_id: <<id::unsigned-integer-size(16)>>}
+  def encode_msg_id(id) when id > 0 and id < (1 <<< 16) do
+    <<id::unsigned-integer-size(16)>>
   end
-  def set_msg_id(_msg, _id), do: raise "Invalid id"
+  def encode_msg_id(_id), do: raise "Invalid id"
 
-  def set_payload(msg, payload) when is_binary(payload) do
-    %Message{msg | payload: payload}
-  end
-  def set_payload(msg, payload), do: set_payload(msg, value_to_binary(payload))
+  def encode_payload(payload) when is_binary(payload), do: payload
+  def encode_payload(payload), do: encode_payload(value_to_binary(payload))
 
   @doc """
   Each option instance in a message specifies the Option Number of the
     defined CoAP option, the length of the Option Value, and the Option
     Value itself.
   """
-  def set_options(msg, options) do
-    options =
-      options
-      |> Stream.map(fn({o, v}) ->
-          cond do
-            is_atom(o) and @options[o] -> {@options[o], v}
-            is_integer(o) -> {o, v}
-            true -> nil
-          end
-        end)
-      |> Stream.reject(&is_nil/1)
-      |> Enum.sort(fn({o1, _v1}, {o2, _v2}) -> o1 < o2 end)
-    %Message{msg | options: build_options(options)}
+  def encode_options(options) do
+    options
+    |> Stream.map(fn({o, v}) ->
+      cond do
+        is_atom(o) and Message.options[o] -> {Message.options[o], v}
+        is_integer(o) -> {o, v}
+        true -> nil
+      end
+    end)
+    |> Stream.reject(&is_nil/1)
+    |> Enum.sort(fn({o1, _v1}, {o2, _v2}) -> o1 < o2 end)
+    |> build_options
   end
 
   @doc """
@@ -192,7 +167,7 @@ defmodule Coapex.Encoder do
   def gen_option_header(value) when value > 12 and value <= 255 do
     {<<13::unsigned-integer-size(4)>>, <<(value-13)::unsigned-integer-size(8)>>}
   end
-  def gen_option_header(value) when value > 255 and value < (2 <<< 16) do
+  def gen_option_header(value) when value > 255 and value < (1 <<< 16) do
     {<<14::unsigned-integer-size(4)>>, <<(value-269)::unsigned-integer-size(16)>>}
   end
   def gen_option_header(_), do: raise "Invalid value"
@@ -202,7 +177,7 @@ defmodule Coapex.Encoder do
     cond do
       value < 0 ->
         raise "invalid value"
-      value > (2 <<< 32) ->
+      value > (1 <<< 32) ->
         raise "invalid value"
       true ->
         number_to_binary(value)
--- test/coapex/encoder_test.exs
+++ test/coapex/encoder_test.exs
@@ -5,46 +5,39 @@ defmodule EncoderTest do
   alias Coapex.Encoder
 
 
-  @host_option Encoder.options[:"Uri-Host"]
-  @path_option Encoder.options[:"Uri-Path"]
-  @port_option Encoder.options[:"Uri-Port"]
+  @host_option Message.options[:"Uri-Host"]
+  @path_option Message.options[:"Uri-Path"]
+  @port_option Message.options[:"Uri-Port"]
 
-  test "Message set type works" do
-    msg = Coapex.Encoder.set_type(%Message{}, :con)
-    assert msg.type == <<0 :: size(2)>>
+  test "encode type works" do
+    assert <<0 :: size(2)>> == Encoder.encode_type(:con)
   end
 
-  test "set Message token works" do
-    msg = Coapex.Encoder.set_token(%Message{}, "abc")
-    assert msg.tk_len == <<3::size(4)>>
-    assert msg.token == "abc"
-    msg = Coapex.Encoder.set_token(%Message{}, "")
-    assert msg.tk_len == <<0::size(4)>>
-    assert msg.token == <<>>
+  test "encode token works" do
+    assert {"abc", <<3::size(4)>>} == Encoder.encode_token("abc")
+    assert {"", <<0::size(4)>>} == Encoder.encode_token("")
   end
 
-  test "set Message code works" do
-    msg = Coapex.Encoder.set_code(%Message{}, {2, 05})
-    assert msg.code == <<2 :: size(3), 5 :: size(5)>>
-
-    msg = Coapex.Encoder.set_code(%Message{}, "2.05")
-    assert msg.code == <<2 :: size(3), 5 :: size(5)>>
-
-    msg = Coapex.Encoder.set_code(%Message{}, 205)
-    assert msg.code == <<2 :: size(3), 5 :: size(5)>>
+  test "encode code works" do
+    assert <<2::size(3), 5::size(5)>> == Encoder.encode_code({2, 05})
+    assert <<2::size(3), 5::size(5)>> == Encoder.encode_code("2.05")
+    assert <<2::size(3), 5::size(5)>> == Encoder.encode_code(205)
   end
-  test "set Message invalid codes raises error" do
-    assert_raise RuntimeError, fn -> Coapex.Encoder.set_code(%Message{}, 123) end
-    assert_raise RuntimeError, fn -> Coapex.Encoder.set_code(%Message{}, 303) end
-    assert_raise RuntimeError, fn -> Coapex.Encoder.set_code(%Message{}, 666) end
-    assert_raise RuntimeError, fn -> Coapex.Encoder.set_code(%Message{}, 700) end
+  test "encode invalid codes raises error" do
+    assert_raise RuntimeError, fn -> Encoder.encode_code(123) end
+    assert_raise RuntimeError, fn -> Encoder.encode_code(303) end
+    assert_raise RuntimeError, fn -> Encoder.encode_code(666) end
+    assert_raise RuntimeError, fn -> Encoder.encode_code(700) end
   end
 
-  test "set Message id works" do
-    msg = Coapex.Encoder.set_msg_id(%Message{}, 1)
-    assert msg.msg_id == <<1::unsigned-integer-size(16)>>
-    msg = Coapex.Encoder.set_msg_id(%Message{}, 100)
-    assert msg.msg_id == <<100::unsigned-integer-size(16)>>
+  test "encode msg id works" do
+    assert <<1::unsigned-integer-size(16)>> == Encoder.encode_msg_id(1)
+    assert <<100::unsigned-integer-size(16)>> == Encoder.encode_msg_id(100)
+  end
+
+  test "encode payload" do
+    assert "abc" == Encoder.encode_payload("abc")
+    assert <<123>> == Encoder.encode_payload(123)
   end
 
   # Now testing options stuff!
@@ -65,7 +58,7 @@ defmodule EncoderTest do
     assert {<<14::size(4)>>, <<(600-15-269)::size(16)>>} == Encoder.gen_option_header(600-15)
   end
 
-  test "build Message options" do
+  test "build options" do
     [delta_urihost, len_urihost] = [@host_option, String.length("foo.bar")]
     [delta_uripath, len_uripath] = [@path_option-delta_urihost, String.length("baz")]
     expected = <<delta_urihost::size(4), len_urihost::size(4), "foo.bar",
@@ -83,36 +76,30 @@ defmodule EncoderTest do
     assert expected == opts
   end
 
-  test "set Message options" do
-    [delta_urihost, len_urihost] = [Encoder.options[:"Uri-Host"], String.length("foo.bar")]
-    [delta_uripath, len_uripath] = [Encoder.options[:"Uri-Path"]-delta_urihost, String.length("baz")]
+  test "set BinaryMessage options" do
+    [delta_urihost, len_urihost] = [Message.options[:"Uri-Host"], String.length("foo.bar")]
+    [delta_uripath, len_uripath] = [Message.options[:"Uri-Path"]-delta_urihost, String.length("baz")]
     expected = <<delta_urihost::size(4), len_urihost::size(4), "foo.bar",
                  delta_uripath::size(4), len_uripath::size(4), "baz">>
-    msg = Coapex.Encoder.set_options(%Message{}, ["Uri-Path": "baz", "Uri-Host": "foo.bar"])
-    assert expected == msg.options
-  end
 
-  test "set Message payload" do
-    msg = Coapex.Encoder.set_payload(%Message{}, "abc")
-    assert "abc" == msg.payload
-    msg = Coapex.Encoder.set_payload(%Message{}, 123)
-    assert <<123>> == msg.payload
+    assert expected == Encoder.encode_options(["Uri-Path": "baz", "Uri-Host": "foo.bar"])
   end
 
   test "encode Coap Message" do
-    msg = Encoder.build_msg({:con, "", "0.01", 11, ["Uri-Path": "baz", "Uri-Host": "foo.bar"], "abc"})
-    assert msg.payload == "abc"
+    msg = %Message{type: :con, token: <<>>, code: "0.01", msg_id: 11,
+                   options: ["Uri-Path": "baz", "Uri-Host": "foo.bar"],
+                   payload: "abc"}
 
-    ver = <<1::size(2)>>
-    type = <<Encoder.types[:con]::size(2)>>
-    tk_len = <<0::size(4)>>
-    code = <<0::size(3), 1::size(5)>>
-    msg_id = <<11::size(16)>>
-    token = <<>>
-    options = Coapex.Encoder.build_options [{@host_option, "foo.bar"}, {@path_option, "baz"}]
-    payload = "abc"
-    expected_msg = <<ver::bitstring, type::bitstring, tk_len::bitstring, code::bitstring,
-                 msg_id::bitstring, token::bitstring, options::bitstring, 0xFF, payload::bitstring>>
+    expected_msg =
+      <<Message.version::bitstring,
+        Message.types[:con]::size(2), # type CON is 0
+        0::size(4),                   # token length is 0
+        0::size(3), 1::size(5),       # code is 0.01
+        11::size(16),                 # message id is 11
+        <<>>,                         # token (empty)
+        Encoder.build_options([{@host_option, "foo.bar"}, {@path_option, "baz"}])::bitstring,
+        0xFF,                         # payload marker
+        "abc">>                       # payload
 
     bin_msg = Encoder.encode(msg)
     assert expected_msg == bin_msg
