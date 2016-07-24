d95254a4c46c79cc3203f736b22f450f0e1cd366
Geovane Fedrecheski <geonnave@gmail.com>
2016-07-24 13:48:56 -0300

    Refactor

--- lib/coapex/server.ex
+++ lib/coapex/server.ex
@@ -3,17 +3,24 @@ defmodule Coapex.Server do
 
   alias Coapex.{Message, Encoder, Decoder}
 
-  @port 9998
+  @default_port 9998
+  @default_router Coapex.Router
 
-  @error_500 Message.init(code: :internal_server_error, type: :ack, msg_id: 1,
-    options: [uri_host: @host, uri_port: @port])
+  @error_500 Message.init(code: :internal_server_error, type: :ack, msg_id: 1)
 
-  def start_link(opts \\ []) do
+  def start_link(opts \\ [router: @default_router]) do
     GenServer.start_link(__MODULE__, opts[:router], name: __MODULE__)
   end
 
+  def delegate(non_conn, router) do
+    msg = non_conn[:msg]
+    path = Keyword.get_values(msg.options, :uri_path)
+    router.match(msg.code, path, non_conn)
+  end
+
+  # GenServer callbacks
   def init(router) do
-    {:ok, socket} = :gen_udp.open(@port, [:binary])
+    {:ok, socket} = :gen_udp.open(@default_port, [:binary])
     {:ok, %{socket: socket, router: router}}
   end
 
@@ -26,10 +33,8 @@ defmodule Coapex.Server do
         out_msg = %Message{in_msg | code: :internal_server_error} |> Encoder.encode
         :gen_udp.send(socket, ip, port, out_msg)
       else
-        data = [udp_params: {socket, ip, port}, msg: in_msg]
-#       spawn(fn -> router.delegate(data) end)
-        state.router.delegate(data)
-        IO.inspect "TEST"
+        non_conn = %{udp: {socket, ip, port}, owner: self(), msg: in_msg}
+        delegate(non_conn, state.router)
       end
     rescue
       e ->
@@ -40,4 +45,33 @@ defmodule Coapex.Server do
     {:noreply, state}
   end
 
+  def handle_info({:reply_con, non_conn}, state) do
+    bin_msg = Coapex.Encoder.encode(non_conn[:msg])
+    {sock, ip, port} = non_conn[:udp]
+    :gen_udp.send(sock, ip, port, bin_msg)
+    {:noreply, state}
+  end
+end
+
+defmodule Coapex.Router do
+  def match(:get, path, non_conn) do
+    if non_conn[:msg].type == :con do
+      reply_con(non_conn, "2.05", "was a get! on path #{path |> inspect}")
+    end
+  end
+  def match(:post, path, non_conn) do
+    if non_conn[:msg].type == :con do
+      reply_con(non_conn, "2.05", "was a post! on path #{path |> inspect}")
+    end
+  end
+  def match(method, path, non_conn) do
+    if non_conn[:msg].type == :con do
+      reply_con(non_conn, "2.05", "was other method: #{method |> inspect} on path #{path |> inspect}")
+    end
+  end
+
+  def reply_con(non_conn, code, payload) do
+    msg = %Coapex.Message{non_conn[:msg] | payload: payload, code: code, type: :ack}
+    send non_conn[:owner], {:reply_con, %{non_conn | msg: msg}}
+  end
 end
--- test/coapex/server_test.exs
+++ test/coapex/server_test.exs
@@ -6,34 +6,8 @@ defmodule ServerTest do
   @host "127.0.0.1"
   @port 9998
 
-  defmodule RouterA do
-    def send_resp(data, code, payload) do
-      {sock, ip, port} = data[:udp_params]
-      msg = data[:msg]
-      msg = %Coapex.Message{msg | payload: payload, code: code}
-      bin_msg = Coapex.Encoder.encode(msg)
-      :gen_udp.send(sock, ip, port, bin_msg)
-    end
-
-    def delegate(data) do
-      msg = data[:msg]
-      path = Keyword.get_values(msg.options, :uri_path)
-      match(msg.code, path, data)
-    end
-
-    def match(:get, path, data) do
-      send_resp(data, "2.05", "was a get! on path #{path |> inspect}")
-    end
-    def match(:post, path, data) do
-      send_resp(data, "2.05", "was a post! on path #{path |> inspect}")
-    end
-    def match(method, path, data) do
-      send_resp(data, "2.05", "was other method: #{method |> inspect} on path #{path |> inspect}")
-    end
-  end
-
   setup do
-    Server.start_link([router: RouterA])
+    Server.start_link
     :ok
   end
 
